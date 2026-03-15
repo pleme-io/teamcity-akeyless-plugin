79cf14ac27f7bcf861dfbeadba8a343fd1cd3c91
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:03 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "TeamCity Akeyless plugin for secret management";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          jdk17
+          gradle
+        ];
+      };
+    });
+}
